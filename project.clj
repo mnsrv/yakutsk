7c264f4cc4275ec228535a72e6c974da0ba94604
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-08-05 05:27:56 +0300

    Add simple web server

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject yakutsk "0.1.0-SNAPSHOT"
+  :dependencies [
+    [org.clojure/clojure        "1.9.0"]
+    [ring/ring-core             "1.7.0-RC1"]
+    [org.immutant/web           "2.1.10"]
+    [compojure                  "1.6.1"]
+  ])
---
+++ src/yakutsk/server.clj
@@ -0,0 +1,22 @@
+(ns yakutsk.server
+  (:require
+    [immutant.web :as web]))
+
+
+(def app
+  (fn [req]
+    { :status 200
+      :body (:uri req) }))
+
+
+(defn -main [& args]
+  (let [args-map (apply array-map args)
+        port-str (or (get args-map "-p")
+                     (get args-map "--port")
+                     "8080")]
+    (web/run #'app { :port (Integer/parseInt port-str) })))
+
+
+(comment
+  (def server (-main "--port" "8080"))
+  (web/stop server))
