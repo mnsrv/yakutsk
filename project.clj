e5cc851b12e88d3252de10d072836ce99b4d425c
Aleksandr Mansurov <mansurov@rocketguys.com>
2018-08-13 11:16:15 +0300

    Render calendar

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject yakutsk "0.1.0-SNAPSHOT"
   :dependencies [
     [org.clojure/clojure        "1.9.0"]
-    [ring/ring-core             "1.7.0-RC1"]
+    [clojure.java-time          "0.3.2"]
     [org.immutant/web           "2.1.10"]
+    [ring/ring-core             "1.7.0-RC1"]
     [compojure                  "1.6.1"]
     [rum                        "0.11.2"]
   ]
--- src/yakutsk/server.clj
+++ src/yakutsk/server.clj
@@ -1,6 +1,7 @@
 (ns yakutsk.server
   (:require
     [rum.core :as rum]
+    [java-time :as time]
     [immutant.web :as web]
     [clojure.java.io :as io]
     [compojure.core :as compojure])
@@ -57,12 +58,64 @@
         [:p "Фронтенд разработчик в Рокетбанке"]]
       [:main children]]])
 
+(def now (time/local-date))
+(def firstDay (time/adjust now :first-day-of-month))
+(def lastDay (time/adjust now :last-day-of-month))
+(def daysInMonth (time/max-value (time/property now :day-of-month)))
+(def localeMonths
+  ["Январь", "Февраль", "Март",
+   "Апрель", "Май", "Июнь",
+   "Июль", "Август", "Сентябрь",
+   "Октябрь", "Ноябрь", "Декабрь"])
+(def month
+  (loop [start 1
+         end (if (time/sunday? firstDay)
+            1
+            (- 7 (time/value (time/day-of-week firstDay))))
+         weeks []]
+    (if (<= start daysInMonth)
+      (recur
+        (inc end)
+        (if (> (+ 7 end) daysInMonth)
+          daysInMonth
+          (+ 7 end))
+        (for [x (range 7)
+              :let [y (* x 3)]
+              :when (even? y)]
+          y)
+      )
+      weeks)))
+month
+
+(time/value (time/day-of-week firstDay))
+(time/year-month now)
+(time/value (time/day-of-week (time/adjust now :first-day-of-month)))
+(time/period 1 :months)
+(time/year-month)
+(time/max-value (time/month))
+(time/day-of-week)
+(Integer/parseInt (time/format "F" (time/adjust now :first-day-of-month)))
+[[#inst "2018-08-12" 2] [3 4]]
+(apply mapv vector [[1 2] [3 4]])
+(vector [1 2] [3 4])
+
+
+(def calendar
+  [:section
+    [:h2 (nth localeMonths (dec (Integer/parseInt (time/format "M" now))))]
+    [:.calendar
+      (for [week [[0 0 1 2 3 4 5]
+                  [6 7 8 9 10 11 12]
+                  [13 14 15 16 17 18 19]
+                  [20 21 22 23 24 25 26]
+                  [27 28 29 30 31 0 0]]]
+        [:.calendar__row
+          (for [day week]
+            [:.calendar__cell (when (> day 0) day)])])]])
+
 
 (rum/defc index [movies]
-  (page "Саша Мансуров"
-    [:section
-      (for [m movies]
-        (movie m))]))
+  (page "Саша Мансуров" calendar))
 
 
 (defn render-html [component]
